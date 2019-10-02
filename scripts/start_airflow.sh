f39645fb796eded86581ea31aa005c446303280f
Maria Sokolova <mlsokolova@gmail.com>
2019-10-02 08:35:57 +0000

    add comments

--- scripts/start_airflow.sh
+++ scripts/start_airflow.sh
@@ -3,7 +3,12 @@ AIRFLOW_BASE=/home/ubuntu/airflow
 VIRTUAL_ENV_DIR=$AIRFLOW_BASE/afenv
 AIRFLOW_HOME=$AIRFLOW_BASE/airflow_home
 source $VIRTUAL_ENV_DIR/bin/activate
+
+#Start DB
 airflow initdb
+
+#Start Webserver
 airflow webserver -D -p 8081 --pid $AIRFLOW_BASE/run/werserver.pid --stdout $AIRFLOW_BASE/logs/webserver/webserver.out --stderr $AIRFLOW_BASE/logs/webserver/webserver.err --log-file $AIRFLOW_BASE/logs/webserver/webserver.log --access_logfile $AIRFLOW_BASE/logs/webserver/access
 
+#Start Scheduler
 airflow scheduler -D  --pid $AIRFLOW_BASE/run/scheduler.pid --stdout $AIRFLOW_BASE/logs/scheduler/scheduler.out --stderr $AIRFLOW_BASE/logs/webserver/scheduler.err --log-file $AIRFLOW_BASE/logs/webserver/scheduler.log
--- scripts/stop_airflow.sh
+++ scripts/stop_airflow.sh
@@ -3,5 +3,9 @@ AIRFLOW_BASE=/home/ubuntu/airflow
 VIRTUAL_ENV_DIR=$AIRFLOW_BASE/afenv
 AIRFLOW_HOME=$AIRFLOW_BASE/airflow_home
 source $VIRTUAL_ENV_DIR/bin/activate
+
+#Stop Webserver
 kill -s TERM `cat $AIRFLOW_BASE/run/werserver.pid`
+
+#Stop Scheduler
 kill -s TERM `cat $AIRFLOW_BASE/run/scheduler.pid`
