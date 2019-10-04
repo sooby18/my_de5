5456f147247d298898d1d3989a9426b9c74edc5f
Maria Sokolova <mlsokolova@gmail.com>
2019-10-04 13:21:41 +0000

    little change

--- scripts/start_airflow.sh
+++ scripts/start_airflow.sh
@@ -11,4 +11,4 @@ airflow initdb
 airflow webserver -D --pid $AIRFLOW_BASE/run/werserver.pid --stdout $AIRFLOW_BASE/logs/webserver/webserver.out --stderr $AIRFLOW_BASE/logs/webserver/webserver.err --log-file $AIRFLOW_BASE/logs/webserver/webserver.log --access_logfile $AIRFLOW_BASE/logs/webserver/access
 
 #Start Scheduler
-airflow scheduler -D  --pid $AIRFLOW_BASE/run/scheduler.pid --stdout $AIRFLOW_BASE/logs/scheduler/scheduler.out --stderr $AIRFLOW_BASE/logs/webserver/scheduler.err --log-file $AIRFLOW_BASE/logs/webserver/scheduler.log
+airflow scheduler -D  --pid $AIRFLOW_BASE/run/scheduler.pid --stdout $AIRFLOW_BASE/logs/scheduler/scheduler.out --stderr $AIRFLOW_BASE/logs/scheduler/scheduler.err --log-file $AIRFLOW_BASE/logs/scheduler/scheduler.log
