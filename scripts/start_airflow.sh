51c647b9a0f85fb047b16bafa5eb3379d8d1180d
Maria Sokolova <mlsokolova@gmail.com>
2019-10-04 08:27:53 +0000

    Airflow DAG

--- scripts/start_airflow.sh
+++ scripts/start_airflow.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-AIRFLOW_BASE=/home/ubuntu/airflow
-VIRTUAL_ENV_DIR=$AIRFLOW_BASE/afenv
-AIRFLOW_HOME=$AIRFLOW_BASE/airflow_home
+export AIRFLOW_BASE=/home/ubuntu/airflow
+export VIRTUAL_ENV_DIR=$AIRFLOW_BASE/afenv
+export AIRFLOW_HOME=$AIRFLOW_BASE/airflow_home
 source $VIRTUAL_ENV_DIR/bin/activate
 
 #Start DB
 airflow initdb
 
 #Start Webserver
-airflow webserver -D -p 8081 --pid $AIRFLOW_BASE/run/werserver.pid --stdout $AIRFLOW_BASE/logs/webserver/webserver.out --stderr $AIRFLOW_BASE/logs/webserver/webserver.err --log-file $AIRFLOW_BASE/logs/webserver/webserver.log --access_logfile $AIRFLOW_BASE/logs/webserver/access
+airflow webserver -D --pid $AIRFLOW_BASE/run/werserver.pid --stdout $AIRFLOW_BASE/logs/webserver/webserver.out --stderr $AIRFLOW_BASE/logs/webserver/webserver.err --log-file $AIRFLOW_BASE/logs/webserver/webserver.log --access_logfile $AIRFLOW_BASE/logs/webserver/access
 
 #Start Scheduler
 airflow scheduler -D  --pid $AIRFLOW_BASE/run/scheduler.pid --stdout $AIRFLOW_BASE/logs/scheduler/scheduler.out --stderr $AIRFLOW_BASE/logs/webserver/scheduler.err --log-file $AIRFLOW_BASE/logs/webserver/scheduler.log
